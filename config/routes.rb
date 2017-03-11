5b188da243a6891635e88e7d4848df5be4f0ce69
chandrakant <chandrakant7892@gmail.com>
2017-03-12 00:01:21 +0530

    reviewer customization

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :products
   devise_for :businessmen
-  devise_for :reviewers
+  devise_for :reviewers, controllers: {
+           :sessions => "reviewers/sessions",
+           :registrations => "reviewers/registrations" }
   devise_for :admins
   root 'home#home_page'
     resources :reviewers
